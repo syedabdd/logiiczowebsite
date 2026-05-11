12848dbd61b4945d06bdf217fe22cd430d7e8bd3
syedabdd <abdullahsyed574@gmail.com>
2026-05-11 19:45:14 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,11 +30,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <Navbar />
+         <div className="flex min-h-screen flex-col items-center justify-center bg-black px-6 text-white">
+      <h1 className="text-7xl font-bold text-red-500">404</h1>
+
+      <h2 className="mt-4 text-3xl font-semibold">
+        Page Not Found
+      </h2>
+
+      <p className="mt-3 max-w-md text-center text-gray-400">
+        Sorry, the page you are looking for doesn’t exist or has been moved.
+      </p>
+      </div>
+        {/* <Navbar />
         <main className="flex-1">
           <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
         </main>
-        <Footer />
+        <Footer /> */}
       </body>
     </html>
   );
